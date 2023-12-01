d3326adc171ac77bc48a5e21f9b10ac21f8b544c
incihuseynli <inci.huseynli0@gmail.com>
2023-12-01 16:35:59 +0400

    Scroll up component created and is ready

---
+++ src/components/Footer-Components/ToTop.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { ArrowSmallUpIcon } from "@heroicons/react/24/solid";
+const ToTop = () => {
+  const [icon, setIcon] = useState<boolean>(false);
+  const toUp = (): void => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  };
+  window.onscroll = function (): void {
+    if (
+      document.body.scrollTop > 30 ||
+      document.documentElement.scrollTop > 30
+    ) {
+      setIcon(true);
+    } else {
+      setIcon(false);
+    }
+  };
+
+  return (
+    <>
+      {icon && (
+        <div
+          onClick={toUp}
+          className="fixed right-4 bottom-4 w-10 h-10 p-2 rounded-full text-white hover:bg-primary-green-800 bg-primary-green-500"
+        >
+          <ArrowSmallUpIcon />
+        </div>
+      )}
+      ;
+    </>
+  );
+};
+
+export default ToTop;
