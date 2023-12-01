90fbf39338a0780d095b71efaa02a8e4b6eb92ca
incihuseynli <inci.huseynli0@gmail.com>
2023-12-01 21:25:44 +0400

    HeroSect created and is ready

---
+++ src/components/Hero-Components/Hero.tsx
@@ -0,0 +1,13 @@
+import HeroSect from "./HeroSect";
+import HeroBorder from "./HeroBorder";
+
+const Hero = () => {
+  return (
+    <>
+      <HeroSect />
+      <HeroBorder />
+    </>
+  );
+};
+
+export default Hero;
