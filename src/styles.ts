370fb341b6273365e56852821fd3f2b5102ad06c
incihuseynli <inci.huseynli0@gmail.com>
2023-12-01 16:34:53 +0400

    Added type

--- src/styles.ts
+++ src/styles.ts
@@ -1,10 +1,12 @@
- const styles = {
-    paddingX : "px-4 md:px-16",
-    paddingY : "py-6 md:py-16",
-    padding: "px-4 md:px-16 py-6",
-    flexBetween: "flex items-center justify-between",
-    flexCenter : "flex items-center justify-center",
-    heading : "text-9xl ",
-}
+import { IStyles } from "./types";
 
-export {styles}
+const styles: IStyles = {
+  paddingX: "px-4 md:px-16",
+  paddingY: "py-6 md:py-16",
+  padding: "px-4 md:px-16 py-6",
+  flexBetween: "flex items-center justify-between",
+  flexCenter: "flex items-center justify-center",
+  heading: "text-9xl ",
+};
+
+export { styles };
--- src/types.tsx
+++ src/types.tsx
@@ -1,9 +1,12 @@
-
+// For Routes
 export type RoutesType = Iroute[];
-
 interface Iroute {
-    id : string | number;
-    path : string;
-    element : JSX.Element;
-    title?: string;
+  id: string | number;
+  path: string;
+  element: JSX.Element;
+  title?: string;
+}
+// For styles.ts
+export interface IStyles {
+  [key: string]: string;
 }
