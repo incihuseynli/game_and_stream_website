05363c1e4480ff554d3bcc5d7c8c725bf3d12b5b
incihuseynli <inci.huseynli0@gmail.com>
2023-12-09 08:44:49 +0400

    new margin rule added

--- src/styles.ts
+++ src/styles.ts
@@ -4,6 +4,7 @@ const styles: IStyles = {
   paddingX: "px-4 md:px-16",
   paddingY: "py-6 md:py-16",
   padding: "px-4 md:px-16 py-6",
+  margin: "mx-4 md:mx-16 my-6",
   flexBetween: "flex items-center justify-between",
   flexCenter: "flex items-center justify-center",
   heading: "text-9xl ",
