e007128693a41797344dd9324cc1cd138c0a59a6
incihuseynli <inci.huseynli0@gmail.com>
2023-12-09 08:49:12 +0400

    Added HOC for games list section

---
+++ src/components/Browse-Components/AllGames.tsx
@@ -0,0 +1,24 @@
+import { styles } from "../../styles";
+import All from "../Global-Components/All";
+import GamesLists from "../Global-Components/GamesLists";
+import SectionHead from "../Global-Components/SectionHead";
+
+const AllGames = () => {
+  return (
+    <section className={`${styles.padding} my-24 flex flex-col gap-16`}>
+      {/* Section Head */}
+      <SectionHead
+        headTitle="Choose game you want"
+        title="All Best Games is in here"
+      />
+      {/* All Games */}
+      <All>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 gap-y-9 place-items-center py-16 px-4 lg:px-8">
+          <GamesLists />
+        </div>
+      </All>
+    </section>
+  );
+};
+
+export default AllGames;
