6fbe154a337af4c29ef8c6be8244c09c86c43b37
incihuseynli <inci.huseynli0@gmail.com>
2023-12-09 09:07:58 +0400

    New paths added

--- src/PageRouting/Paths.tsx
+++ src/PageRouting/Paths.tsx
@@ -1,7 +1,16 @@
 import Home from "../pages/Home";
 import Browse from "../pages/Browse";
-import {RoutesType} from "../types";
-export const Paths:RoutesType = [
+import { RoutesType } from "../types";
+import GameDetail from "../pages/GameDetail";
+import Tournament from "../pages/Tournament";
+import Streams from "../pages/Streams";
+import Contact from "../pages/Contact";
+import TournamentDetails from "../pages/TournamentDetails";
+import StreamerDetails from "../pages/StreamerDetails";
+import PageNotFound from "../pages/PageNotFound";
+import Auth from "../pages/Auth";
+import PasswordRepair from "../components/Auth-Components/PasswordRepair";
+export const Paths: RoutesType = [
   {
     id: 1,
     path: "/",
@@ -17,19 +26,56 @@ export const Paths:RoutesType = [
   {
     id: 3,
     path: "/tournament",
-    element: <Browse />,
+    element: <Tournament />,
     title: "Tournament",
   },
   {
     id: 4,
     path: "/streams",
-    element: <Browse />,
+    element: <Streams />,
     title: "Streams",
   },
   {
     id: 5,
     path: "/contact",
-    element: <Browse />,
+    element: <Contact />,
     title: "Contact",
   },
+  {
+    id: 6,
+    path: "/browse/:id",
+    element: <GameDetail />,
+  },
+  {
+    id: 7,
+    path: "/tournament/:id",
+    element: <TournamentDetails />,
+  },
+  {
+    id: 8,
+    path: "/streams/:id",
+    element: <StreamerDetails />,
+  },
+  {
+    id:9,
+    path : "/auth",
+    element : <Auth/>
+  },
+  {
+    id:10,
+    path : "/auth/lost-password",
+    element : <PasswordRepair/>
+  },
+  {
+    id: 12,
+    path: "*",
+    element: <PageNotFound/>
+  }
+  /**
+   * TODO: Streamers and Streamer Page
+   * TODO: Contact Page ✔
+   * TODO: Sign in/Log in Page
+   * TODO: Profile Page
+   * TODO: Error Page ✔
+   */
 ];
