c0cf65216b55c6817d2915a3b2dd193e8b40cdbf
incihuseynli <inci.huseynli0@gmail.com>
2023-12-09 08:56:20 +0400

    HOC created and is ready

---
+++ src/components/Global-Components/TopList.tsx
@@ -0,0 +1,16 @@
+import { IHoc } from "../../types";
+
+const TopList = ({ secTitle, children }:IHoc) => {
+  return (
+    <div className="flex flex-col items-center gap-4 w-full bg-black-500 rounded-2xl p-6">
+      {/* HEAD */}
+      <h4 className="mb-4 text-3xl text-white font-barlow font-bold tracking-wider">
+        {secTitle}
+      </h4>
+      {/* LIST */}
+      {children}
+    </div>
+  );
+};
+
+export default TopList;
