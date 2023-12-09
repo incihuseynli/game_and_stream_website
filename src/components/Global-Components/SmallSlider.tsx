df25a96370249c762ca912151a1974d10deec200
incihuseynli <inci.huseynli0@gmail.com>
2023-12-09 08:54:57 +0400

    HOC created and is ready

---
+++ src/components/Global-Components/SmallSlider.tsx
@@ -0,0 +1,50 @@
+import { Swiper } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Autoplay } from "swiper/modules";
+import CustomSwiperPagination from "./CustomSwiperPagination";
+import { styles } from "../../styles";
+import useMediaQuery from "../../hooks/useMediaQuery";
+import { useEffect, useState } from "react";
+import { IHoc } from "../../types";
+const SmallSlider = ({ secTitle, children }:IHoc) => {
+  const isAboveMediumScreen = useMediaQuery("(min-width:768px)");
+  const [slidesPerView, setSlidesPerView] = useState<number>(1);
+  useEffect(() => {
+    if (isAboveMediumScreen) {
+      setSlidesPerView(3);
+    } else {
+      setSlidesPerView(1);
+    }
+  }, [isAboveMediumScreen]);
+
+  return (
+    <div className="w-full h-[500px] bg-black-500 text-white  rounded-xl">
+      <Swiper
+        slidesPerView={slidesPerView}
+        spaceBetween={30}
+        loop={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        modules={[Autoplay]}
+        className="mySwiper globalSlider p-10  relative"
+      >
+        {/* HEAD OF SLIDER */}
+        <div
+          className={`w-full ${styles.flexBetween} absolute top-4 right-0 left-0 ${styles.paddingX} `}
+        >
+          <h4 className="text-2xl text-white font-barlow font-bold tracking-wider">
+            {secTitle}
+          </h4>
+          <CustomSwiperPagination />
+        </div>
+        {/* SLIDES */}
+        {children}
+      </Swiper>
+    </div>
+  );
+};
+
+export default SmallSlider;
