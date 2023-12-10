5a9f1f5a8b5e8954f38179f2f829611f17b17814
incihuseynli <inci.huseynli0@gmail.com>
2023-12-10 20:51:47 +0400

    data fetched via json-server url

--- src/components/Browse-Components/SingleGame.tsx
+++ src/components/Browse-Components/SingleGame.tsx
@@ -5,129 +5,106 @@ import {
 } from "@heroicons/react/24/solid";
 
 import { styles } from "../../styles";
-import img1 from "/img1.jpg";
+
 import BtnText from "../Buttons/BtnText";
-import GameDetailAPI from "../../api/GameDetailAPI";
+import { useEffect, useState } from "react";
+import Axios from "axios";
+import { useParams } from "react-router";
+import { IGame } from "../../types";
+
 const SingleGame = () => {
-  /**
-   * ! TOO MANY REQUEST ERROR
-   * ! COULDN'T GET ANY DATA FROM GAME
-   * ! GAME RETURNS NULL
-   */
+  const { id } = useParams<{ id: string }>();
+  const [game, setGame] = useState<IGame[]>([]);
+  const URL = "http://localhost:3080/games/";
+  // "https://my-json-server.typicode.com/dbForProjects/games_api/games/";
+  const handleData = () => {
+    try {
+      Axios.get(URL + id).then(({ data }) => {
+        setGame(data);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    handleData();
+  }, []);
+  const smallImages = game.smallImgs;
   return (
-    <GameDetailAPI>
-      {(game) => (
+    <div
+      className={`${styles.flexCenter} flex-col gap-12 ${styles.paddingY} px-4 bg-black-500 rounded-xl`}
+    >
+      <div className="flex  items-center justify-center w-full">
+        <img
+          src={game.thumbnail}
+          alt={game.title}
+          className="w-full md:w-fit h-fit  lg:w-fit lg:h-[22.5rem] object-cover rounded-xl"
+        />
+      </div>
+      {/* About Game */}
+      <div className="flex flex-col items-center justify-center gap-10">
+        <h4 className="text-3xl md:text-4xl text-white font-barlow font-bold tracking-wide">
+          <span className="text-primary-green-500">{game.title}</span> Details
+        </h4>
+
         <div
-          className={`${styles.flexCenter} flex-col gap-12 ${styles.paddingY} px-4 bg-black-500 rounded-xl`}
+          className={`${styles.flexCenter} flex-wrap gap-8 py-8 px-16 bg-black-800  rounded-xl text-base capitalize tracking-wider font-poppins font-bold`}
         >
-          {/* Image and Game Promo Video */}
-          {game ? (
-            <>
-              <div className="flex flex-col items-center md:flex-row gap-6">
-                <img
-                  src={game.thumbnail}
-                  alt={game.title}
-                  className="w-full md:w-fit h-fit  lg:w-fit lg:h-[22.5rem] object-cover rounded-xl"
-                />
-                <video
-                  className="w-96 h-fit lg:max-w-fit  lg:h-[22.5rem] rounded-xl"
-                  controls
-                >
-                  <source src="your-video-source.mp4" type="video/mp4" />
-                  <source src="your-video-source.webm" type="video/webm" />
-                  <source src="your-video-source.ogv" type="video/ogg" />
-                  Your browser does not support the video tag.
-                </video>
-              </div>
-              {/* About Game */}
-              <div className="flex flex-col items-center justify-center gap-10">
-                <h4 className="text-3xl md:text-4xl text-white font-barlow font-bold tracking-wide">
-                  <span className="text-primary-green-500">{game.title}</span>{" "}
-                  Details
-                </h4>
-
-                <div
-                  className={`${styles.flexCenter} flex-wrap gap-8 py-8 px-16 bg-black-800  rounded-xl text-base capitalize tracking-wider font-poppins font-bold`}
-                >
-                  <span className="text-gray-500">{game.publisher}</span>
-                  <span className="flex items-center gap-2 text-gray-50 font-bold font-poppins text-sm">
-                    <div className="w-5 h-5 text-secondary-yellow-500">
-                      <StarIcon />
-                    </div>{" "}
-                    4.8
-                  </span>
-                  <span className="border border-opacity-10 border-gray-500 p-2 text-black">
-                    13+
-                  </span>
-                  <span className="text-secondary-yellow-500">Action</span>
-                  <div className={`${styles.flexCenter} gap-2`}>
-                    <div className="w-5 h-5 text-primary-green-500 font-bold">
-                      <ArrowDownTrayIcon />
-                    </div>
-                    <span className="text-gray-50 font-bold font-poppins text-sm">
-                      2.5M
-                    </span>
-                  </div>
-                  <div className="flex items-center gap-2">
-                    <div className="w-5 h-5 text-primary-green-300">
-                      <CircleStackIcon />
-                    </div>
-                    <span className="text-white">36.5GB</span>
-                  </div>
-                </div>
-
-                <div className={`${styles.flexCenter} flex-col  gap-6`}>
-                  <div className={`${styles.flexCenter} flex-wrap gap-6`}>
-                    <img
-                      src={img1}
-                      alt=""
-                      className="w-full md:w-72 h-fit object-cover rounded-xl"
-                    />
-                    <img
-                      src={img1}
-                      alt=""
-                      className="w-full md:w-72 h-fit object-cover rounded-xl"
-                    />
-                    <img
-                      src={img1}
-                      alt=""
-                      className="w-full md:w-72 h-fit object-cover rounded-xl"
-                    />
-                  </div>
-                  {/* About Game */}
-                  <div className="w-full md:w-3/4 flex flex-col items-center gap-4">
-                    <p className="text-gray-500 font-poppins tracking-wider text-sm md:text-base">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Alias, dignissimos obcaecati? Consequatur tempora officiis
-                      aspernatur vitae nemo illum, ratione alias aliquam ullam
-                      quod quibusdam mollitia? Lorem ipsum dolor sit amet
-                      consectetur adipisicing elit. Alias, dignissimos
-                      obcaecati? Consequatur tempora officiis aspernatur vitae
-                      nemo illum, ratione alias aliquam ullam quod quibusdam
-                      mollitia? Lorem ipsum dolor sit amet consectetur
-                      adipisicing elit. Alias, dignissimos obcaecati?
-                      Consequatur tempora officiis aspernatur vitae nemo illum,
-                      ratione alias aliquam ullam quod quibusdam mollitia? Lorem
-                      ipsum dolor sit amet consectetur adipisicing elit. Alias,
-                      dignissimos obcaecati? Consequatur tempora officiis
-                      aspernatur vitae nemo illum, ratione alias aliquam ullam
-                      quod quibusdam mollitia? Lorem ipsum dolor sit amet
-                      consectetur adipisicing elit. Alias, dignissimos
-                      obcaecati? Consequatur tempora officiis aspernatur vitae
-                      nemo illum, ratione alias aliquam ullam quod quibusdam
-                      mollitia?
-                    </p>
-                    <BtnText children="Download game now" path="/" />
-                  </div>
-                </div>
-              </div>
-            </>
-          ) : (
-            <div className="text-white"> ERROR</div>
-          )}
+          <span className="text-gray-500">{game.publisher}</span>
+          <span className="flex items-center gap-2 text-gray-50 font-bold font-poppins text-sm">
+            <div className="w-5 h-5 text-secondary-yellow-500">
+              <StarIcon />
+            </div>{" "}
+            {game.rate}
+          </span>
+          <span className="border border-opacity-10 border-gray-500 p-2 text-black">
+            {game.age_rate}+
+          </span>
+          <span className="text-secondary-yellow-500">{game.genre}</span>
+          <div className={`${styles.flexCenter} gap-2`}>
+            <div className="w-5 h-5 text-primary-green-500 font-bold">
+              <ArrowDownTrayIcon />
+            </div>
+            <span className="text-gray-50 font-bold font-poppins text-sm">
+              {game.downloads}
+            </span>
+          </div>
+          <div className="flex items-center gap-2">
+            <div className="w-5 h-5 text-primary-green-300">
+              <CircleStackIcon />
+            </div>
+            <span className="text-white">{game.storage}</span>
+          </div>
         </div>
-      )}
-    </GameDetailAPI>
+
+        <div className={`${styles.flexCenter} flex-col  gap-6`}>
+          <div className={`${styles.flexCenter} flex-wrap gap-6`}>
+            {smallImages && smallImages.length > 0 && (
+              <div className={`${styles.flexCenter} flex-wrap gap-6`}>
+                {smallImages.map(({ img }, index) => (
+                  <img
+                    key={index}
+                    src={img}
+                    alt=""
+                    className="w-full md:w-44 h-32 object-cover rounded-xl"
+                  />
+                ))}
+              </div>
+            )}
+          </div>
+          {/* About Game */}
+          <div className="w-full md:w-3/4 flex flex-col items-center gap-4">
+            <p className="text-gray-500 font-poppins tracking-wider text-sm md:text-base">
+              {Array.from({ length: 4 }, (_, index) => (
+                <span key={index}>{game.content}</span>
+              ))}
+            </p>
+
+            <BtnText children="Download game now" path={game.game_url} />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
