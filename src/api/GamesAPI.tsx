975de8a8ab0647c8a64d1213dd5b2f7a11fd8f5f
incihuseynli <inci.huseynli0@gmail.com>
2023-12-10 20:50:38 +0400

    API changed to json-server url

--- src/api/GamesAPI.tsx
+++ src/api/GamesAPI.tsx
@@ -3,21 +3,14 @@ import { useEffect, useState } from "react";
 import { GamesAPIProps, IGame } from "../types";
 
 const GamesAPI: React.FC<GamesAPIProps> = ({ limit, children }) => {
-  const options = {
-    method: "GET",
-    url: "https://free-to-play-games-database.p.rapidapi.com/api/games",
-    headers: {
-      "X-RapidAPI-Key": "8270335a98mshff2fc09f55e7c03p15f4a7jsn3afb95e7043a",
-      "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
-    },
-  };
+  const URL = `http://localhost:3080/games?_limit=${limit}`;
+  // `https://my-json-server.typicode.com/dbForProjects/games_api/games?_limit=${limit}`;
   const [games, setGames] = useState<IGame[]>([]);
-  const handleData = async () => {
+  const handleData = () => {
     try {
-      const response = await Axios.request(options);
-      const limitedGames = response.data.slice(0, limit);
-
-      setGames(limitedGames);
+      Axios.get(URL).then(({ data }) => {
+        setGames(data);
+      });
     } catch (error) {
       console.error(error);
     }
