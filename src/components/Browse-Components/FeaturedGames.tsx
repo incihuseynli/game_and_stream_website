90a4f54e94d0b642cbbc5a9d77a3c2e0239867e3
incihuseynli <inci.huseynli0@gmail.com>
2023-12-20 18:27:04 +0400

    getting data from API

--- src/components/Browse-Components/FeaturedGames.tsx
+++ src/components/Browse-Components/FeaturedGames.tsx
@@ -1,166 +1,80 @@
-import { ArrowDownTrayIcon, StarIcon } from "@heroicons/react/24/solid";
+import {
+  StarIcon,
+  UserIcon,
+} from "@heroicons/react/24/solid";
 import { styles } from "../../styles";
 import { SwiperSlide } from "swiper/react";
 import SmallSlider from "../Global-Components/SmallSlider";
+import { useEffect, useState } from "react";
+import { IStreamers } from "../../types";
+import axios from "axios";
+import { NavLink } from "react-router-dom";
 const FeaturedGames = () => {
+  const URL = "http://localhost:3080/streamers";
+  const [streamers, setStreamers] = useState<IStreamers[]>([]);
+  useEffect(() => {
+    axios.get(URL).then(({ data }) => {
+      setStreamers(data);
+    });
+  }, []);
   return (
     <SmallSlider secTitle="Streamer Featured Games">
-      <SwiperSlide className="rounded-2xl h-80 my-12 relative">
-        <img src="./streamerSlider1.jpg" alt="" className="rounded-2xl" />
-        <div className="p-2">
-          <div className={`${styles.flexBetween} w-full`}>
-            <h6 className="text-base capitalize font-barlow font-bold text-white">
-              Gamer Name
-            </h6>
-            {/* Stars and rate is here */}
-            <div className={`${styles.flexCenter} gap-2`}>
-              <div className="w-5 h-5 text-secondary-yellow-500">
-                <StarIcon />
-              </div>
-              <span className="text-gray-50 font-bold font-poppins text-sm">
-                4.8
-              </span>
-            </div>
-          </div>
-          {/* Watchers and count is here */}
-          <div className={`${styles.flexBetween} w-full`}>
-            {/* Game Name  */}
-            <h6 className="text-gray-500 text-xs capitalize tracking-wider font-poppins font-bold">
-              Game Name
-            </h6>
-            {/* Download rate */}
+      {streamers.map(
+        ({
+          id,
+          username,
+          game_name,
+          link,
+          rate,
+          thumbnail,
+          monthly_watchers,
+          followers,
+        }) => {
+          return (
+            <SwiperSlide key={id} className="rounded-2xl h-80 my-12 relative">
+              <NavLink to={link}>
+                <img src={thumbnail} alt="" className="rounded-2xl" />
+              </NavLink>
+              <div className="p-2">
+                <div className={`${styles.flexBetween} w-full`}>
+                  <h6 className="text-base capitalize font-barlow font-bold text-white">
+                    {username}
+                  </h6>
+                  {/* Stars and rate is here */}
+                  <div className={`${styles.flexCenter} gap-2`}>
+                    <div className="w-5 h-5 text-secondary-yellow-500">
+                      <StarIcon />
+                    </div>
+                    <span className="text-gray-50 font-bold font-poppins text-sm">
+                      {rate}
+                    </span>
+                  </div>
+                </div>
+                {/* Watchers and count is here */}
+                <div className={`${styles.flexBetween} w-full`}>
+                  {/* Game Name  */}
+                  <h6 className="text-gray-500 text-xs capitalize tracking-wider font-poppins font-bold">
+                    {game_name}
+                  </h6>
+                  {/* Download rate */}
 
-            <div className={`${styles.flexCenter} gap-2`}>
-              <div className="w-3 h-3 text-primary-green-500 font-bold">
-                <ArrowDownTrayIcon />
+                  <div className={`${styles.flexCenter} gap-2`}>
+                    <div className="w-3 h-3 text-primary-green-500 font-bold">
+                      <UserIcon />
+                    </div>
+                    <span className="text-gray-50 font-bold font-poppins text-xs">
+                      {followers}
+                    </span>
+                  </div>
+                </div>
               </div>
-              <span className="text-gray-50 font-bold font-poppins text-xs">
-                2.5M
-              </span>
-            </div>
-          </div>
-        </div>
-        <div className="text-white font-bold font-barlow tracking-wider text-base capitalize absolute -bottom-6 translate-x-1/3 hidden swiperHover p-2 bg-primary-green-300">
-          2.4K Streaming
-        </div>
-      </SwiperSlide>
-      <SwiperSlide className="rounded-2xl h-80 my-12 relative">
-        <img src="./streamerSlider1.jpg" alt="" className="rounded-2xl" />
-        <div className="p-2">
-          <div className={`${styles.flexBetween} w-full`}>
-            <h6 className="text-base capitalize font-barlow font-bold text-white">
-              Gamer Name
-            </h6>
-            {/* Stars and rate is here */}
-            <div className={`${styles.flexCenter} gap-2`}>
-              <div className="w-5 h-5 text-secondary-yellow-500">
-                <StarIcon />
+              <div className="text-white font-bold font-barlow tracking-wider text-base capitalize absolute -bottom-6 left-10 hidden swiperHover p-2 bg-primary-green-300">
+                {monthly_watchers} Streaming
               </div>
-              <span className="text-gray-50 font-bold font-poppins text-sm">
-                4.8
-              </span>
-            </div>
-          </div>
-          {/* Watchers and count is here */}
-          <div className={`${styles.flexBetween} w-full`}>
-            {/* Game Name  */}
-            <h6 className="text-gray-500 text-xs capitalize tracking-wider font-poppins font-bold">
-              Game Name
-            </h6>
-            {/* Download rate */}
-
-            <div className={`${styles.flexCenter} gap-2`}>
-              <div className="w-3 h-3 text-primary-green-500 font-bold">
-                <ArrowDownTrayIcon />
-              </div>
-              <span className="text-gray-50 font-bold font-poppins text-xs">
-                2.5M
-              </span>
-            </div>
-          </div>
-        </div>
-        <div className="text-white font-bold font-barlow tracking-wider text-base capitalize absolute -bottom-6 translate-x-1/3 hidden swiperHover p-2 bg-primary-green-300">
-          2.4K Streaming
-        </div>
-      </SwiperSlide>
-      <SwiperSlide className="rounded-2xl h-80 my-12 relative">
-        <img src="./streamerSlider1.jpg" alt="" className="rounded-2xl" />
-        <div className="p-2">
-          <div className={`${styles.flexBetween} w-full`}>
-            <h6 className="text-base capitalize font-barlow font-bold text-white">
-              Gamer Name
-            </h6>
-            {/* Stars and rate is here */}
-            <div className={`${styles.flexCenter} gap-2`}>
-              <div className="w-5 h-5 text-secondary-yellow-500">
-                <StarIcon />
-              </div>
-              <span className="text-gray-50 font-bold font-poppins text-sm">
-                4.8
-              </span>
-            </div>
-          </div>
-          {/* Watchers and count is here */}
-          <div className={`${styles.flexBetween} w-full`}>
-            {/* Game Name  */}
-            <h6 className="text-gray-500 text-xs capitalize tracking-wider font-poppins font-bold">
-              Game Name
-            </h6>
-            {/* Download rate */}
-
-            <div className={`${styles.flexCenter} gap-2`}>
-              <div className="w-3 h-3 text-primary-green-500 font-bold">
-                <ArrowDownTrayIcon />
-              </div>
-              <span className="text-gray-50 font-bold font-poppins text-xs">
-                2.5M
-              </span>
-            </div>
-          </div>
-        </div>
-        <div className="text-white font-bold font-barlow tracking-wider text-base capitalize absolute -bottom-6 translate-x-1/3 hidden swiperHover p-2 bg-primary-green-300">
-          2.4K Streaming
-        </div>
-      </SwiperSlide>
-      <SwiperSlide className="rounded-2xl h-80 my-12 relative">
-        <img src="./streamerSlider1.jpg" alt="" className="rounded-2xl" />
-        <div className="p-2">
-          <div className={`${styles.flexBetween} w-full`}>
-            <h6 className="text-base capitalize font-barlow font-bold text-white">
-              Gamer Name
-            </h6>
-            {/* Stars and rate is here */}
-            <div className={`${styles.flexCenter} gap-2`}>
-              <div className="w-5 h-5 text-secondary-yellow-500">
-                <StarIcon />
-              </div>
-              <span className="text-gray-50 font-bold font-poppins text-sm">
-                4.8
-              </span>
-            </div>
-          </div>
-          {/* Watchers and count is here */}
-          <div className={`${styles.flexBetween} w-full`}>
-            {/* Game Name  */}
-            <h6 className="text-gray-500 text-xs capitalize tracking-wider font-poppins font-bold">
-              Game Name
-            </h6>
-            {/* Download rate */}
-
-            <div className={`${styles.flexCenter} gap-2`}>
-              <div className="w-3 h-3 text-primary-green-500 font-bold">
-                <ArrowDownTrayIcon />
-              </div>
-              <span className="text-gray-50 font-bold font-poppins text-xs">
-                2.5M
-              </span>
-            </div>
-          </div>
-        </div>
-        <div className="text-white font-bold font-barlow tracking-wider text-base capitalize absolute -bottom-6 translate-x-1/3 hidden swiperHover p-2 bg-primary-green-300">
-          2.4K Streaming
-        </div>
-      </SwiperSlide>
+            </SwiperSlide>
+          );
+        }
+      )}
     </SmallSlider>
   );
 };
--- src/components/Browse-Components/SimilarGameSection.tsx
+++ src/components/Browse-Components/SimilarGameSection.tsx
@@ -1,25 +1,51 @@
-import BtnText from "../Buttons/BtnText"
-import ScrollCarouselSlider from "../Global-Components/ScrollCarouselSlider"
+import { useEffect, useState } from "react";
+import BtnText from "../Buttons/BtnText";
 import SectionHead from "../Global-Components/SectionHead";
-import image from "/img1.jpg";
+import ScrollCarousel from "scroll-carousel-react";
+import { IGame } from "../../types";
+import axios from "axios";
 
 const SimilarGameSection = () => {
+  const URL = "http://localhost:3080/games";
+  const [games, setGames] = useState<IGame[]>([]);
+  useEffect(() => {
+    axios.get(URL).then(({ data }) => {
+      setGames(data);
+    });
+  }, []);
   return (
-    <section className="mt-36 flex flex-col items-center gap-16">
-        {/* Section Head */}
-        <SectionHead headTitle="Play more" title="similar games" />
-        {/* Slider */}
-       <ScrollCarouselSlider>
-        <img src={image} alt="" className="w-32 h-32 rounded-xl" />
-        <div className="">
-          <h6 className="text-lg font-barlow font-bold text-white truncate">
-            Game Name
-          </h6>
-          <BtnText children="play now" path="/" />
-        </div>
-      </ScrollCarouselSlider>
+    <section className="mt-36 flex flex-col items-center gap-16 py-16 bg-black-500 rounded-lg">
+      {/* Section Head */}
+      <SectionHead headTitle="Play more" title="similar games" />
+      {/* Slider */}
+      <ScrollCarousel autoplay autoplaySpeed={600} speed={2} smartSpeed={true}>
+        {[1, 2].map((item) => (
+          <div key={item} className="w-full h-80 flex gap-6 mr-6">
+            {games.map(({ id, title, thumbnail }) => {
+              return (
+                <div
+                  key={id}
+                  className="w-56 h-64 text-white bg-black-800 border border-primary-green-800 hovered rounded-xl flex flex-col items-center justify-center gap-4"
+                >
+                  <img
+                    src={thumbnail}
+                    alt={` Play ${title}`}
+                    className="w-32 h-32 rounded-xl object-cover"
+                  />
+                  <div className="flex flex-col items-center justify-center">
+                    <h6 className="text-lg font-barlow font-bold text-white  w-fit truncate">
+                      {title}
+                    </h6>
+                    <BtnText children="play now" path={`/browse/${id}`} />
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        ))}
+      </ScrollCarousel>
     </section>
-  )
-}
+  );
+};
 
-export default SimilarGameSection
+export default SimilarGameSection;
