180906df5b6523bda3cca7924905a82f47ad1b3a
incihuseynli <inci.huseynli0@gmail.com>
2023-12-20 18:29:47 +0400

    Getting datas

--- src/components/Global-Components/GamesLists.tsx
+++ src/components/Global-Components/GamesLists.tsx
@@ -2,7 +2,6 @@ import { StarIcon } from "@heroicons/react/24/solid";
 import { styles } from "../../styles";
 import { ArrowDownTrayIcon } from "@heroicons/react/24/solid";
 import BtnText from "../Buttons/BtnText";
-// import { IGame } from "../../types";
 import GamesAPI from "../../api/GamesAPI";
 import { IGame } from "../../types";
 
@@ -12,60 +11,63 @@ const GamesLists = () => {
       <GamesAPI limit={6}>
         {(games: IGame[]) => (
           <>
-            {games.map(({ id, title, thumbnail, publisher, game_url }) => {
-              return (
-                <div
-                  key={id}
-                  className={`flex flex-col md:flex-row gap-4 items-center w-full h-full   rounded-xl p-4 bg-black-800 shadow-2xl border border-white border-opacity-10 hover:border hover:border-primary-green-500 hover:border-opacity-30`}
-                >
-                  <img
-                    src={thumbnail}
-                    alt="Game Name"
-                    className="rounded-xl w-40 h-40  object-cover"
-                  />
-                  <div className="w-full flex flex-col gap-2">
-                    {/* Top */}
-                    <div className={`flex flex-col items-start  w-full`}>
-                      {/* Game Name */}
-                      <h5 className="text-white text-lg uppercase tracking-wide font-barlow font-bold truncate w-40">
-                        {title}
-                      </h5>
-
-                      <div className={`${styles.flexCenter} gap-4`}>
-                        {/* Game Rate */}
-                        <div className={`${styles.flexCenter} gap-2`}>
-                          <div className="w-5 h-5 text-secondary-yellow-500">
-                            <StarIcon />
+            {games.map(
+              ({ id, title, thumbnail, publisher, rate, downloads }) => {
+                return (
+                  <div
+                    key={id}
+                    className={`mx-6 md:mx-0 flex flex-col md:flex-row gap-4 items-center w-full h-full   rounded-xl p-4 bg-black-800 shadow-2xl border border-white border-opacity-10 hover:border hover:border-primary-green-500 hover:border-opacity-30`}
+                  >
+                    <img
+                      src={thumbnail}
+                      alt={title}
+                      className="rounded-xl w-40 h-40 lg:w-40 lg:h-40 md:w-32 md:h-32 object-cover"
+                    />
+                    <div className="w-full flex flex-col md:items-start items-center gap-2">
+                      {/* Top */}
+                      <div className={`flex flex-col w-full`}>
+                        {/* Game Name */}
+                        <h5 className="text-center md:text-left text-white text-lg md:text-base lg:text-lg uppercase tracking-wide font-barlow font-bold truncate w-full md:w-40">
+                          {title}
+                        </h5>
+                        <div
+                          className={`flex items-center mx-auto md:mx-0 gap-4 md:items-start`}
+                        >
+                          {/* Game Rate */}
+                          <div className={`${styles.flexCenter} gap-2`}>
+                            <div className="w-5 h-5 text-secondary-yellow-500">
+                              <StarIcon />
+                            </div>
+                            <span className="text-gray-50 font-bold font-poppins text-sm">
+                              {rate}
+                            </span>
                           </div>
-                          <span className="text-gray-50 font-bold font-poppins text-sm">
-                            4.8
-                          </span>
-                        </div>
-                        {/* Download rate */}
-                        <div className={`${styles.flexCenter} gap-2`}>
-                          <div className="w-5 h-5 text-primary-green-500 font-bold">
-                            <ArrowDownTrayIcon />
+                          {/* Download rate */}
+                          <div className={`${styles.flexCenter} gap-2`}>
+                            <div className="w-5 h-5 text-primary-green-500 font-bold">
+                              <ArrowDownTrayIcon />
+                            </div>
+                            <span className="text-gray-50 font-bold font-poppins text-sm">
+                              {downloads}
+                            </span>
                           </div>
-                          <span className="text-gray-50 font-bold font-poppins text-sm">
-                            2.5M
-                          </span>
                         </div>
                       </div>
+                      {/* Bottom */}
+                      <div
+                        className={`${styles.flexBetween}  md:flex-col md:items-start lg:flex-row lg:items-center  mb-4 w-full`}
+                      >
+                        {/* Game Company  */}
+                        <h5 className="text-gray-500 text-xs truncate text-center md:text-left w-full md:w-36 mr-2 capitalize tracking-wider font-poppins font-bold">
+                          {publisher}
+                        </h5>
+                      </div>
+                      <BtnText path={`/browse/${id}`}>More Details</BtnText>
                     </div>
-                    {/* Bottom */}
-                    <div
-                      className={`${styles.flexBetween}  md:flex-col md:items-start lg:flex-row lg:items-center  mb-4 w-full`}
-                    >
-                      {/* Game Company  */}
-                      <h5 className="text-gray-500 text-xs truncate w-40 mr-2 capitalize tracking-wider font-poppins font-bold">
-                        {publisher}
-                      </h5>
-                    </div>
-                    <BtnText path={`/browse/${id}`}>More Details</BtnText>
                   </div>
-                </div>
-              );
-            })}
+                );
+              }
+            )}
           </>
         )}
       </GamesAPI>
--- src/components/Global-Components/Stats.tsx
+++ src/components/Global-Components/Stats.tsx
@@ -3,6 +3,8 @@ import { styles } from "../../styles";
 import ScrollTrigger from "react-scroll-trigger";
 import CountUp from "react-countup";
 import SectionHead from "./SectionHead";
+import Reveal from "../../transitions/Reveal";
+import { stats } from "../../Db/datas";
 
 const Stats = () => {
   const [counterOn, setCounterOn] = useState<boolean>(false);
@@ -11,75 +13,39 @@ const Stats = () => {
       {/* Section Head */}
       <SectionHead headTitle="Our services" title="Just enjoy playing" />
       {/* Stats */}
-      <div
-        className={`${styles.flexCenter} py-12 px-6 rounded-2xl bg-black-500 flex-wrap w-3/4 gap-16 mx-auto `}
-      >
-        {/* number of players */}
-        <div className="w-40 font-barlow font-bold flex flex-col items-center text-center  md:border-r md:border-gray-500 md:border-opacity-30 md:pr-24">
-          <img src="./Playstation.svg" alt="" className="w-10 h-10" />
+      <Reveal>
+        <div
+          className={`${styles.flexCenter} py-12 px-6 rounded-2xl bg-black-500 flex-wrap w-3/4 gap-16 mx-auto `}
+        >
+          {stats.map(({ id, image, title, end }) => {
+            return (
+              <div
+                className="w-40 font-barlow font-bold flex flex-col items-center text-center md:even:border-none md:even:pr-0 lg:even:border-r lg:even:pr-24 md:last:border-none  md:last:pr-0 md:border-r md:border-gray-500 md:border-opacity-30 md:pr-24"
+                key={id}
+              >
+                <img
+                  src={image}
+                  alt={`number of ${title}`}
+                  className="w-10 h-10"
+                />
 
-          <ScrollTrigger
-            onEnter={() => setCounterOn(true)}
-            onExit={() => setCounterOn(false)}
-          >
-            <span className="text-3xl text-white">
-              {counterOn && (
-                <CountUp start={0} end={1368} duration={3} delay={0} />
-              )}
-              +
-            </span>
-          </ScrollTrigger>
-          <h6 className="text-base text-gray-300 uppercase">Players</h6>
+                <ScrollTrigger
+                  onEnter={() => setCounterOn(true)}
+                  onExit={() => setCounterOn(false)}
+                >
+                  <span className="text-3xl text-white">
+                    {counterOn && (
+                      <CountUp start={0} end={end} duration={3} delay={0} />
+                    )}
+                    +
+                  </span>
+                </ScrollTrigger>
+                <h6 className="text-base text-gray-300 uppercase">{title}</h6>
+              </div>
+            );
+          })}
         </div>
-        {/* number of streamers */}
-        <div className="w-40 font-barlow font-bold flex flex-col items-center text-center  md:border-r md:border-gray-500 md:border-opacity-30 md:pr-24">
-          <img src="./streamer.svg" alt="" className="w-10 h-10" />
-          <ScrollTrigger
-            onEnter={() => setCounterOn(true)}
-            onExit={() => setCounterOn(false)}
-          >
-            <span className="text-3xl text-white">
-              {counterOn && (
-                <CountUp start={0} end={569} duration={3} delay={0} />
-              )}
-              +
-            </span>
-          </ScrollTrigger>
-          <h6 className="text-base text-gray-300 uppercase">Streamer</h6>
-        </div>
-        {/* number of games */}
-        <div className="w-40 font-barlow font-bold flex flex-col items-center text-center  md:border-r md:border-gray-500 md:border-opacity-30 md:pr-24">
-          <img src="./dice.svg" alt="" className="w-10 h-10" />
-          <ScrollTrigger
-            onEnter={() => setCounterOn(true)}
-            onExit={() => setCounterOn(false)}
-          >
-            <span className="text-3xl text-white">
-              {counterOn && (
-                <CountUp start={0} end={748} duration={3} delay={0} />
-              )}
-              +
-            </span>
-          </ScrollTrigger>
-          <h6 className="text-base text-gray-300 uppercase">Games</h6>
-        </div>
-        {/* number of tournaments */}
-        <div className="w-40 font-barlow font-bold flex flex-col items-center">
-          <img src="./Cup.svg" alt="" className="w-10 h-10" />
-          <ScrollTrigger
-            onEnter={() => setCounterOn(true)}
-            onExit={() => setCounterOn(false)}
-          >
-            <span className="text-3xl text-white">
-              {counterOn && (
-                <CountUp start={0} end={327} duration={3} delay={0} />
-              )}
-              +
-            </span>
-          </ScrollTrigger>
-          <h6 className="text-base text-gray-300 uppercase">Tournaments</h6>
-        </div>
-      </div>
+      </Reveal>
     </section>
   );
 };
