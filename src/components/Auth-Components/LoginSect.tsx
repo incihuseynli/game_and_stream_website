d4775147ce7113cd824359f597f7e63752f794ce
incihuseynli <inci.huseynli0@gmail.com>
2023-12-20 18:23:38 +0400

    Password show function added

--- src/components/Auth-Components/LoginSect.tsx
+++ src/components/Auth-Components/LoginSect.tsx
@@ -1,7 +1,48 @@
-import { EyeSlashIcon } from "@heroicons/react/24/solid";
 import { NavLink } from "react-router-dom";
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/solid";
+import { useEffect, useState } from "react";
+import { FormEvent, IUsers } from "../../types";
+import Cookies from "js-cookie";
+import SubmitBtn from "../Buttons/SubmitBtn";
+type LoginSectProps = {
+  onLogin: () => void;
+};
+const LoginSect: React.FC<LoginSectProps> = ({ onLogin }) => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [rememberMe, setRememberMe] = useState<boolean>(true);
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  // Show Password Toggle
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
 
-const LoginSect = () => {
+  // Login config
+  const handleLogin = (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      const existingUsers = JSON.parse(localStorage.getItem("users") || "[]");
+      const isUserExist = existingUsers.some(
+        (user: IUsers) => user.email === email && user.password == password
+      );
+
+      if (isUserExist) {
+        onLogin();
+        if (rememberMe) {
+          Cookies.set("loggedIn", "true", { expires: 7 });
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const loggedInCookie = Cookies.get("loggedIn");
+    if (loggedInCookie) {
+      onLogin();
+    }
+  }, []);
   return (
     <section className="border border-gray-500 border-opacity-20 p-8 flex flex-col gap-4 shadow-xl">
       <h4 className="text-3xl font-barlow font-bold uppercase text-white">
@@ -11,27 +52,49 @@ const LoginSect = () => {
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto,
         facilis.
       </p>
-      <form className="flex flex-col gap-6 ">
+      <form className="flex flex-col gap-6 " onSubmit={handleLogin}>
         <input
           type="email"
           placeholder="Email *"
-          className="w-full border border-gray-400 border-opacity-20 bg-black-800 p-4 rounded-sm outline-none focus:border-primary-green-300 transition-all duration-700 text-base font-poppins tracking-wide capitalize text-white"
+          className="w-full border border-gray-400 border-opacity-20 bg-black-800 p-4 rounded-sm outline-none focus:border-primary-green-300 transition-all duration-700 text-base font-poppins tracking-wide  text-white"
+          value={email}
+          required
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
         />
         <div className="w-full flex items-center border border-gray-400 border-opacity-20 bg-black-800 rounded-sm outline-none focus-within:border-primary-green-300 transition-all duration-700 text-base font-poppins tracking-wide capitalize text-white">
           <input
-            type="password"
+            type={showPassword ? "text" : "password"}
             placeholder="Password *"
             className="border-none p-4 mr-2 w-full h-full outline-none bg-transparent flex-1"
+            value={password}
+            required
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
           />
-          <EyeSlashIcon className="w-4 h-4 mr-2 text-gray-500" />
+          {showPassword ? (
+            <EyeIcon
+              className="w-4 h-4 mr-2 text-gray-500"
+              onClick={handleShowPassword}
+            />
+          ) : (
+            <EyeSlashIcon
+              className="w-4 h-4 mr-2 text-gray-500"
+              onClick={handleShowPassword}
+            />
+          )}
         </div>
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-2">
             <input
               type="checkbox"
-              name=""
-              id=""
               className="accent-primary-green-300 w-4 h-4"
+              aria-checked="true"
+              required
+              checked={rememberMe}
+              onChange={() => setRememberMe(!rememberMe)}
             />
             <span className="text-sm font-poppins font-medium text-white">
               Remember me
@@ -44,11 +107,8 @@ const LoginSect = () => {
             Lost your password?
           </NavLink>
         </div>
-        <input
-          type="submit"
-          value="Log in"
-          className="mt-6 w-40 text-white font-barlow font-bold tracking-wide uppercase text-base bg-primary-green-500 hover:bg-secondary-yellow-500 transition-all duration-700 py-4 px-8 rounded-sm"
-        />
+
+        <SubmitBtn value="Log in" />
       </form>
     </section>
   );
